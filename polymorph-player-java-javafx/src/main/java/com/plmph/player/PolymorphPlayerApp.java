1b46625a6340ca5e2bedd408085751ccfd0341a2
Jakob Jenkov <fake@jenkov.com>
2026-03-06 20:15:59 +0100

    Adds first version of the Polymorph Player in Java and JavaFX #13

---
+++ polymorph-player-java-javafx/src/main/java/com/plmph/player/PolymorphPlayerApp.java
@@ -0,0 +1,20 @@
+package com.plmph.player;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Polymorph Player JavaFX App
+ */
+public class PolymorphPlayerApp extends Application {
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage rootStage) {
+        rootStage.setTitle("Polymorph Player");
+        rootStage.show();
+    }
+}
---
+++ polymorph-player-java-javafx/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.plmph.player {
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires javafx.fxml;
+    requires javafx.web;
+    exports com.plmph.player;
+}
